5a45ec00d51e9e61ea7642c1310945d0b1935fa3
Soumen Bose <soumen.bose11@gmail.com>
2021-08-21 10:32:02 +0530

    Add files via upload

---
+++ src/main/java/com/soumen/ratinginfoservice/RatinginfoserviceApplication.java
@@ -0,0 +1,13 @@
+package com.soumen.ratinginfoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatinginfoserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatinginfoserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soumen/ratinginfoservice/RatinginfoserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soumen.ratinginfoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatinginfoserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
