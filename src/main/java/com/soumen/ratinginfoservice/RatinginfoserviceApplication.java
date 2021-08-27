8c5e27d92a8db4d8cbc3a4584d123a8dafb9f61d
251119 <soumen.bose11@outlook.com>
2021-08-27 18:34:53 +0530

    commit

--- src/main/java/com/soumen/ratinginfoservice/RatinginfoserviceApplication.java
+++ src/main/java/com/soumen/ratinginfoservice/RatinginfoserviceApplication.java
@@ -2,8 +2,10 @@ package com.soumen.ratinginfoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class RatinginfoserviceApplication {
 
 	public static void main(String[] args) {
